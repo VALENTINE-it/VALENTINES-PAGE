75b1f20961c07280ac6c44001f3d553dc3289ea8
valeomondi <valentineomondi2005@gmail.com>
2026-08-04 10:44:01 +0300

    added a var.go code

---
+++ Beginner/var.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "Awili"
+	age := 21
+	height := 1.37
+	isStudent := true
+
+	fmt.Printf("My name is %s, I am %d years old, my height is %.2f meters, and I am a student: %t.\n", name, age, height, isStudent)
+}
